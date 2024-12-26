8e2d8eb4bc2503262c298bfa00609c4c70f3d450
ketan gandhi <gandhi.ketan55555@gmail.com>
2024-12-26 15:04:42 +0530

    frontend setup

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -1,7 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     darkMode: ["class"],
-    content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
+    content: [
+		"./index.html",
+		"./src/**/*.{js,ts,jsx,tsx}",
+	  ],
   theme: {
   	extend: {
   		borderRadius: {
